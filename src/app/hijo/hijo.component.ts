44b48a1c4b8c540330ddf60a527d3ed20a57f2c3
Santiag0gopenux2024 <michael.martinez@gopenux.com>
2024-08-07 11:15:55 -0500

    Se agregaron validaciones al campo de nombre

--- src/app/hijo/hijo.component.ts
+++ src/app/hijo/hijo.component.ts
@@ -10,6 +10,11 @@ export class HijoComponent {
   @Output() newItemEvent = new EventEmitter<string>();
 
   onAddNewItem(item: string): void {
-    this.newItemEvent.emit(item);
+    const pattern = /^[a-zA-Z\s]+$/;
+    if (pattern.test(item)) {
+      this.newItemEvent.emit(item);
+    } else {
+      console.error('El valor ingresado contiene caracteres no permitidos.');
+    }
   }
 }
