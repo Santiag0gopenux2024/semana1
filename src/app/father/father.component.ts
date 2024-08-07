b03f2f93a8b5a100b79828d584446e9319885fb3
Santiag0gopenux2024 <michael.martinez@gopenux.com>
2024-08-07 14:02:16 -0500

    Se realizo la implementacion de los Componentes (ngOnChanges, ngOnInit, ngAfterViewInit, ngOnDestroy)

--- src/app/father/father.component.ts
+++ src/app/father/father.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, } from '@angular/core';
 
 @Component({
   selector: 'app-father',
   templateUrl: './father.component.html',
   styleUrls: ['./father.component.css']
 })
-export class FatherComponent {
+
+export class FatherComponent implements OnChanges {
   receivedName: string = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente Padre ha detectado cambios', changes);
+  }
 
   onNewItemReceived(name: string): void {
     this.receivedName = name;
--- src/app/son/son.component.ts
+++ src/app/son/son.component.ts
@@ -1,13 +1,31 @@
-import {Component, Output, EventEmitter } from '@angular/core';
+import {Component, Output, EventEmitter, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-son',
   templateUrl: './son.component.html',
   styleUrls: ['./son.component.css']
 })
-export class SonComponent {
+
+export class SonComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @Output() newItemEvent = new EventEmitter<string>();
+  private subscription: Subscription | undefined;
+
+  ngOnInit(): void {
+    console.log('Componente Hijo inicializado');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Componente Hijo inicializó la vista');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente Hijo destruido');
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 
   onAddNewItem(item: string): void {
     const pattern = /^[a-zA-Z\s]+$/;
